51038673704ddf132bf3cc2a83f54b17dd5556f5
Orlando3011 <34520084+Orlando3011@users.noreply.github.com>
2019-12-03 19:41:05 +0100

    Added simple plot

--- Project1/src/__init__.py
+++ Project1/src/__init__.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("data.txt")
 stats = df.describe()
+
+df.plot(kind='scatter', x='epss', y='lvdd')
+plt.savefig("scatterplot1")
 
 document = stats.to_html("statistics.html")
 
@@ -9,15 +13,14 @@ outputFile = open("statistics.html", "r")
 
 data = outputFile.read()
 
-
 outputFile.close()
 
 head = "<!DOCTYPE=html>\n<html>\n<head>Basic information about echocardiogram data</head>\n<body>\n"
+image = "<img src='scatterplot1.png'>"
 tail = "</body></html>"
-data = head + data + tail
+data = head + data + image + tail
 
 outputFile = open("statistics.html", "w")
 
 outputFile.write(data)
-
 outputFile.close()
